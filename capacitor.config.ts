21c9a5c774ccfdc1edb99f30e9f1e9b5633203d5
ZSASZMANRASTA <martinzsasz@gmail.com>
2025-10-04 08:55:35 +0300

    Added Capacitor config and Android platform setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kajiado.cosmetics',
+  appName: 'Kajiado POS',
+  webDir: 'dist'
+};
+
+export default config;
